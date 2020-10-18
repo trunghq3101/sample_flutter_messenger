e91daa34e8ae8e7916f70b74df141a6e2a6382b6
TrungH <trunghq3101@gmail.com>
2020-10-18 08:01:24 +0700

    [c] Update app name + Add applicationIdSuffix for debug variant

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:redesign_telegram/main.dart';
+import 'package:sample_flutter_messenger/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
